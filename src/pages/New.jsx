777d7f7ae0bdb1e631289f0d8b9457830ba19c55
hr.noh <hr.noh@quintet.co.kr>
2024-08-28 13:56:12 +0900

    New 화면 구성

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,7 +1,14 @@
+import Header from "../components/Header.jsx";
+import Button from "../components/Button.jsx";
+import Editor from "../components/Editor.jsx";
+
 const New = () =>{
   return (
       <div>
-        New
+        <Header title={"새 일기 쓰기"}
+           leftChild={<Button text={"< 뒤로가기"}/>}
+        />
+        <Editor />
       </div>
   );
 }
